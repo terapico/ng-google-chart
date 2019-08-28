053c02dd305cdd017d490216fc511e406aea2a9f
Jeffrey Sze <terapico@gmail.com>
2019-08-28 14:50:48 -0700

    Added formated value to Elead

--- src/app/components/dash-ssi/dash-ssi.component.ts
+++ src/app/components/dash-ssi/dash-ssi.component.ts
@@ -11,7 +11,7 @@ export class DashSsiComponent implements OnInit {
   c1data = [
     ['SSI', 956, "B1", 950, 955, 967, "A1"],
     ['CSI', 928, "B1", 940, 932, 940, "A2"],
-    ['ELead', (950/5.2*4.2), "B1", 950, (950/5.2*2.8),(950/5.2*3), "A3"],
+    ['ELead', {v: (950/5.2*4.2), f: "4.2%"}, "B1", {v: 950, f:"5.2%"}, {v: (950/5.2*2.8), f:"2.8%"},{v:(950/5.2*3), f:"3%"}, "A3"],
   ];
   c1columnNames = ["Type", 'Dealer', {type:"string", role: "annotation"}, 'Dist', "Reg", "Natl", {type:"string", role: "annotation"}];
   c1options = {    
